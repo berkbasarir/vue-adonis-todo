f9638916c9ae1393f57e48395eb25def0d718e8d
berkbasarir <bbasarir@gmail.com>
2021-06-24 00:38:35 +0300

    destroy with exceptions

---
+++ server/app/Services/AuthorizationService.js
@@ -0,0 +1,16 @@
+const ResourceNotExistException = use("App/Exceptions/ResourceNotExistException");
+const InvalidAccessException = use('App/Exceptions/InvalidAccessException');
+
+class AuthorizationService {
+    verifyPermission(resource, user){
+        if (!resource) {
+            throw new ResourceNotExistException();
+        }
+
+        if (resource.user_id !== user.id){
+            throw new InvalidAccessException();
+        }
+    }
+}
+
+module.exports = new AuthorizationService();
